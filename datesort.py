6c6c7bb2415065f11d138d92cb8af9443e4c8953
DrNemo1571497 <44184042+DrNemo1571497@users.noreply.github.com>
2018-11-14 10:42:54 -0800

    Update datesort.py

--- datesort.py
+++ datesort.py
@@ -1,11 +1,7 @@
+# This program sorts the dates from oldest to newest
+
+
 import datetime
-"""
-timestamps = ['2010-01-12', '2011-01-12, 2009-02-12']
-dates = [datetime.datetime.strptime(ts, "%Y-%m-%d") for ts in timestamps]
-dates.sort()
-sorteddates = [datetime.datetime.strftime(ts, "%Y-%m-%d") for ts in dates]
-sorteddates
-"""
 
 import time
 timestamps = ['01 Mar 2017', '03 Feb 2117', '15 Jan 1998', '28 Dec 1993']
